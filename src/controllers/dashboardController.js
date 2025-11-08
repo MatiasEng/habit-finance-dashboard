6109d3b6576527611a05b9505933046a1beefd26
MatiasEng <m4ti4s.404@gmail.com>
2025-11-07 21:03:21 -0300

    Feat: Completing implementation of userController & dashboard | Not Tested

--- src/controllers/dashboardController.js
+++ src/controllers/dashboardController.js
@@ -1,4 +1,49 @@
+import users from '../data/users.js';
+import habits from '../data/habits.js';
+import expenses from '../data/expenses.js';
+// Return the entire dashboard
+/*
+User info: username, email, admin
+Habits info: title, category, streak, best streak, completed dates
+Expense info: category, description, amount, date => total spend
+
+*/
 function getEntireDashboard(req, res) {
+  const user = req.user;
+  const userHabits = habits.filter(h => h.userId === user.id);
+  const userExpenses = expenses.filter(e => e.userId === user.id);
+  
+  const habitsInfo = userHabits.map(h => ({
+    title: h.title,
+    category: h.category,
+    streak: h.streak,
+    bestStreak: h.bestStreak,
+    completedDates: h.completedDates
+  }));
+
+  const totalSpend = userExpenses.reduce((sum, expense) => sum + expense.amount, 0);
+  
+  const expensesInfo = userExpenses.map(e => ({
+    category: e.category,
+    description: e.description,
+    amount: e.amount,
+    date: e.date
+
+  }));
+  
+  
+  res.json({
+    UserInfo: {
+      username: user.username,
+      email: user.email,
+      isAdmin: user.isAdmin
+    }, 
+    HabitsInfo: habitsInfo, 
+    ExpensesInfo: {
+      Expenses: expensesInfo,
+      total: totalSpend
+    }
+  });
 
 }
 
--- src/controllers/userControler.js
+++ src/controllers/userControler.js
@@ -1,15 +1,43 @@
 import users from "../data/users.js";
+import habits from '../data/habits.js';
+import expenses from '../data/expenses.js';
+
 
 function getMyProfile(req, res) {
   res.json(req.user);
-
 }
 
 function updateProfile(req, res) {
+  const user  = req.user;
+  const { username, email, password } = req.body;
+  
+  if (!username && !email && !password) {
+    return res.status(400).json({error: "At least one field is require"});
+  }
+  
+  user.username = username ?? user.username;
+  user.email = email ?? user.email;
+  user.password = password ?? user.password;
   
 }
 
 function deleteAccount(req, res) {
+  const userId = req.user.id;
+
+  // delete the habits linked to the user
+  // Keep all the habits which userId is different that the userId that we are deleting
+  habits = habits.filter(h => h.userId !== userId);
+
+  // delete the expenses linked to the user
+  // Keep all the expenses which userId is different that the userId that we are deleting
+  expenses = expenses.filter(e => e.userId !== userId); 
+
+  // delete the user 
+  const userIndex = users.findIndex(u => u.id === userId);
+  if (userIndex === -1) return res.status(404).json({error: "User not Found"});
+  
+  const deletedUser = users.splice(userIndex,1);
+  res.json(deletedUser);
   
 }
 
